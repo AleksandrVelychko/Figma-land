d5d6e1ff38c6d44ea79dbe38b7abc5fb769b822f
AleksandrVelychko <oleksandr.velychko96@gmail.com>
2021-05-24 17:07:16 +0300

    initial

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function($){
+	$(document).ready(function() {
+		$('.header__burger').click(function(event) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock');
+		});
+
+		AOS.init();
+	});
+})(jQuery);
